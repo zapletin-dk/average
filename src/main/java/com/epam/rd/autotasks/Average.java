903b66d712d017f762aae7da20b1421f90cac77b
Zapletin Daniil <Zaplot1994@gmail.com>
2022-12-22 18:51:16 +0300

    Task completed with all tests passed

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        int sum = 0;
+        int counter =0;
+        int digit  = scanner.nextInt();
+        while (digit != 0){
+            sum += digit;
+            digit = scanner.nextInt();
+            counter++;
+        }
+        System.out.println(sum/counter);
     }
 
 }
