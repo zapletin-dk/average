c0ddebd3aab14845d478b28027f34d938c3e85fc
Zapletin Daniil <Zaplot1994@gmail.com>
2023-03-06 21:16:23 +0300

    Renamed some variables

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int counter =0;
+        int sumOfDigits = 0;
+        int counterOfDigits = 0;
         int digit  = scanner.nextInt();
         while (digit != 0){
-            sum += digit;
+            sumOfDigits += digit;
             digit = scanner.nextInt();
-            counter++;
+            counterOfDigits++;
         }
-        System.out.println(sum/counter);
+        System.out.println(sumOfDigits / counterOfDigits);
     }
-
 }
